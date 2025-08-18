c411a6ca2626ed782ad57b079632f7e61871f62e
Mohammad Sameer <mohammadsameer7651@gmail.com>
2025-08-18 22:48:57 +0530

    Moved Gym Management System into its own folder

---
+++ Chat-Application/firebase-config.js
@@ -0,0 +1,14 @@
+const getFirebaseConfig = () => {
+  return {
+    apiKey: "AIzaSyAxq0JAt-uy7rSseQw3QTirukw-sFzf-NA",
+    authDomain: "chat-application-um.firebaseapp.com",
+    databaseURL: "https://chat-application-um-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "chat-application-um",
+    storageBucket: "chat-application-um.appspot.com",
+    messagingSenderId: "174354703374",
+    appId: "1:174354703374:web:a00b5ce4245574e124c85f",
+    measurementId: "G-V780M4L7KT"
+  };
+};
+
+export { getFirebaseConfig };
